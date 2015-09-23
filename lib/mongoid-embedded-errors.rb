4add4cd70154c34f79b5db8781ef0b13da23cec9
Ji Zhou <calf.zhou@gmail.com>
2015-09-23 09:48:04 +0800

    fix bug that `rel.metadata` not exists

--- lib/mongoid-embedded-errors.rb
+++ lib/mongoid-embedded-errors.rb
@@ -25,7 +25,7 @@ module Mongoid
             # get each of their individual message and add them to the parent's errors:
             if rel.errors.any?
               rel.errors.messages.each do |k, v|
-                key = (rel.metadata.relation == Mongoid::Relations::Embedded::Many ? "#{name}[#{i}].#{k}" : "#{name}.#{k}").to_sym
+                key = (metadata.relation == Mongoid::Relations::Embedded::Many ? "#{name}[#{i}].#{k}" : "#{name}.#{k}").to_sym
                 errs.delete(key)
                 errs[key] = v
                 errs[key].flatten!
