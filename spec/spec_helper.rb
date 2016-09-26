0ef8ceb75707f4b173db277bfb3b9121ebe77480
Kristijan Novoselic <kristijan@glooko.com>
2016-09-27 01:04:15 +0200

    Update DatabaseCleaner config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,10 +5,12 @@ require 'database_cleaner'
 Mongoid.load!(File.join(File.dirname(__FILE__), 'config.yml'), :test)
 require File.join(File.dirname(__FILE__), 'support', 'models')
 
-DatabaseCleaner[:mongoid].strategy = :truncation
-
 RSpec.configure do |config|
-  config.before(:each) do
-    DatabaseCleaner.clean
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning { example.run }
   end
 end
