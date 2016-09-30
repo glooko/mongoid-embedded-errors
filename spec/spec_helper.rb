7ce72ae90de525342b66eaf533c4fde04ec34b70
Kristijan Novoselic <kristijan@glooko.com>
2016-09-30 12:07:26 +0200

    Additional RSpec configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,20 @@ Dir[File.join(current_path, 'support/**/*.rb')].each { |f| require f }
 Mongoid.load! File.join(current_path, 'support/mongoid.yml'), :test
 
 RSpec.configure do |config|
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+  config.filter_run_excluding :skip
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+    expectations.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+  config.disable_monkey_patching!
+
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
   end
