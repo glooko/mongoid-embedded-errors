408a4ed193a7d0dba800bb0f60b69a025bc45b63
lstosic <luka.stosic94@gmail.com>
2019-02-11 09:56:44 +0100

    Fix code and add rspec tests by using gem mutant

--- spec/lib/mongoid/embedded_errors_spec.rb
+++ spec/lib/mongoid/embedded_errors_spec.rb
@@ -4,6 +4,22 @@ RSpec.describe Mongoid::EmbeddedErrors do
 
     before { |spec| article.validate unless spec.metadata[:do_not_validate] }
 
+    context 'when module is already included in a class', :do_not_validate do
+      let(:dummy_class) do
+        Class.new do
+          include Mongoid::Document
+          include Mongoid::EmbeddedErrors
+        end
+      end
+
+      it 'does not create errors_without_embedded_errors alias again' do
+        a = dummy_class.instance_method(:errors_without_embedded_errors)
+        dummy_class.include described_class
+
+        expect(a).to eq(dummy_class.instance_method(:errors_without_embedded_errors))
+      end
+    end
+
     context 'when article does not have any pages associated' do
       let(:pages) { [] }
 
@@ -23,12 +39,29 @@ RSpec.describe Mongoid::EmbeddedErrors do
       it 'returns all errors for `pages[0]` object' do
         expect(article.errors[:'pages[0].title']).to include "can't be blank"
       end
+      it "returns `can't be blank` error for `pages[0]` object" do
+        expect(article.errors[:'pages[0].title']).to eq(["can't be blank"])
+      end
     end
 
-    context 'when all pages in article are valid' do
-      let(:pages) { [Page.new(title: 'First page')] }
+    context 'when embeds_many relation is invalid' do
+      let(:pages) { [Page.new(title: 'Test page', sections: sections)] }
+      let(:sections) { [Section.new] }
 
-      it { is_expected.to be_valid }
+      it 'returns all errors for `sections[0]` object' do
+        expect(article.errors[:'pages[0].sections[0].header']).to include "can't be blank"
+      end
+    end
+
+    context 'when embeds_one relation is invalid' do
+      subject(:article) { Article.new name: 'Test', summary: '-', pages: pages, annotation: annotation }
+
+      let(:pages) { [Page.new(title: 'Test page')] }
+      let(:annotation) { Annotation.new }
+
+      it 'returns all errors for `annotation` object' do
+        expect(article.errors[:'annotation.text']).to include "can't be blank"
+      end
     end
 
     context 'when embedded document has not been validated', :do_not_validate do
