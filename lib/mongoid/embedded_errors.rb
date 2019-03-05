f1ce0d8ed140de86c894b2fad7ad197504fefd5a
lstosic <38382611+lstosic@users.noreply.github.com>
2019-03-05 14:20:16 +0100

    Add mutant to the CI, fix some of the alive mutations
    * Fix code and add rspec tests by using gem mutant
    
    * Fix rspec test and remove unnecessary comment

--- lib/mongoid/embedded_errors.rb
+++ lib/mongoid/embedded_errors.rb
@@ -6,35 +6,31 @@ module Mongoid::EmbeddedErrors
   def self.included(klass)
     return if klass.instance_methods.include?(:errors_without_embedded_errors)
 
-    klass.send :alias_method, :errors_without_embedded_errors, :errors
-    klass.send :alias_method, :errors, :errors_with_embedded_errors
+    klass.public_send :alias_method, :errors_without_embedded_errors, :errors
+    klass.public_send :alias_method, :errors, :errors_with_embedded_errors
   end
 
   def errors_with_embedded_errors
     errors_without_embedded_errors.tap do |errs|
       embedded_relations.each do |name, metadata|
-        # name is something like pages or sections
-        # if there is an 'is invalid' message for the relation then let's work it:
-        next unless Array(public_send(name)).any? { |doc| doc.errors.any? }
-
         # first delete the unless 'is invalid' error for the relation
         errs[name].delete 'is invalid'
-        errs.delete name.to_sym if errs[name].empty?
         # next, loop through each of the relations (pages, sections, etc...)
-        [send(name)].flatten.reject(&:nil?).each_with_index do |rel, i|
+        [public_send(name)].flatten.reject(&:nil?).each_with_index do |rel, i|
           # get each of their individual message and add them to the parent's errors:
-          next unless rel.errors.any?
-
-          rel.errors.messages.each do |k, v|
+          rel.errors.each do |k, v|
             relation = if Gem::Version.new(Mongoid::VERSION) >= Gem::Version.new('7.0.0')
                          metadata.class
                        else
                          metadata.relation
                        end
-            key = (relation == EMBEDS_MANY ? "#{name}[#{i}].#{k}" : "#{name}.#{k}").to_sym
+            key = if relation.equal? EMBEDS_MANY
+                    "#{name}[#{i}].#{k}"
+                  else
+                    "#{name}.#{k}"
+                  end.to_sym
             errs.delete(key)
-            errs.add key, v.flatten
-            errs[key].flatten!
+            errs.add key, v
           end
         end
       end
